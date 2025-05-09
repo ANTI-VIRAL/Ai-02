b7c19102c669fa0508cd1900ad9b0cdfa8226292
ANTI VIRAL <109937537+ANTI-VIRAL@users.noreply.github.com>
2025-05-09 21:31:58 +0800

    Update set-firewall.sh

--- set-firewall.sh
+++ set-firewall.sh
@@ -1,8 +1,6 @@
-
 #!/bin/bash
 
-# IP dan PORT VPS RELAY kamu
-RELAY_IP="45.66.221.0"
+# PORT VPS RELAY kamu
 RELAY_PORT="12263"
 
 # FLUSH tapi jaga koneksi SSH biar gak lost
@@ -18,8 +16,8 @@ sudo iptables -P FORWARD DROP
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
 
-# Allow relay pool proxy
-sudo iptables -A OUTPUT -p tcp -d $RELAY_IP --dport $RELAY_PORT -j ACCEPT
+# Allow koneksi keluar ke port relay (destination ALL)
+sudo iptables -A OUTPUT -p tcp --dport $RELAY_PORT -j ACCEPT
 
 # Allow DNS (kalau pakai domain)
 sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
